28a1d76cad8d9230ff618324daaf86ed7f3d5195
Luis <luisfaro013@gmail.com>
2022-09-07 14:46:32 +0200

    cambios cards Luis

--- src/pages/Personajes/Personajes.jsx
+++ src/pages/Personajes/Personajes.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Card } from '../../components/Card/Card.jsx'
+import "./Personajes.css"
 
 //PINTAR DATOS PARA LUIS
 
 export const Personajes = () => {
   return (
-    <div>Personajes</div>
+    <div class='container'>
+      <Card name='Morty' />
+      <Card name='Morty2' />
+    </div>
   )
 }
