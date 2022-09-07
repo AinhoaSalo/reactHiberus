78bb08e7f1928781f7c046b04e6fac343d8851ba
Julen Mendioroz <jmendiorozredin@gmail.com>
2022-09-07 14:21:49 +0200

    DELETE logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Home } from './pages/Home/Home';
-import { Episodios } from './pages//Episodios/Episodios';
-import { Personajes } from './pages/Personajes/Personajes';
-import { NavBar } from './components/NavBar/NavBar';
-import { Footer } from './components/Footer/Footer';
-import { Formulario } from './pages/Formularios/Formulario';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
+import "./App.css"
+import { NavBar } from "./components/NavBar/NavBar"
+import { Footer } from "./components/Footer/Footer"
+import { Home } from "./pages/Home/Home"
+import { Episodios } from "./pages//Episodios/Episodios"
+import { Personajes } from "./pages/Personajes/Personajes"
+import { Formulario } from "./pages/Formularios/Formulario"
 
 //https://mycolor.space/?hex=%23004165&sub=1 paleta colores elegida Dotting Palette
 //Colores: Verde #D4E586, azul chillon #00DEEA azul mas oscuro tirando a gris #94B0B2 y gris marron #AAAD9A
@@ -20,10 +20,10 @@ function App() {
         </header>
         <main>
           <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/episodios' element={<Episodios />} />
-            <Route exact path='/personajes' element={<Personajes />} />
-            <Route exact path='/contacto' element={<Formulario />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/episodios" element={<Episodios />} />
+            <Route exact path="/personajes" element={<Personajes />} />
+            <Route exact path="/contacto" element={<Formulario />} />
           </Routes>
         </main>
         <footer>
@@ -31,7 +31,7 @@ function App() {
         </footer>
       </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
