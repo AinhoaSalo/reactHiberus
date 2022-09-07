78ee079170ed4caa0d4e345c0feb8fb1c39d327a
AinhoaSalo <94933683+AinhoaSalo@users.noreply.github.com>
2022-09-07 18:01:26 +0200

    Proceso media querys

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import './Home.css'
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
   return (
     <>
       <section className='todoHome'>
-        <div className='seccionesHome'>
+        <Link className='seccionesHome' to='/personajes'>
           <h2 className='tituloSeccionesHome'>Personajes</h2>
           <img className='imgSeccionesHome' src="https://fondosmil.com/fondo/27345.jpg" alt="Personajes" />
-        </div>
-        <div className='seccionesHome'>
+        </Link>
+        <Link className='seccionesHome' to='/episodios'>
           <h2 className='tituloSeccionesHome'>Episodios</h2>
           <img className='imgSeccionesHome' src="https://p4.wallpaperbetter.com/wallpaper/588/5/300/rick-and-morty-toilets-hd-wallpaper-preview.jpg" alt="Episodios" />
-        </div>
-        <div className='seccionesHome'>
+        </Link>
+        <Link className='seccionesHome' to='/'>
           <h2 className='tituloSeccionesHome'>Juego memoria</h2>
           <img className='imgSeccionesHome' src="https://areajugones.sport.es/wp-content/uploads/2021/09/rick-y-morty-3.jpg" alt="Juego cartas" />
-        </div>
+        </Link>
       </section>
     </>
   )
