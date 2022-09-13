2586b104f30e16fcc14a40927f36ed078a77e8f2
AinhoaSalo <94933683+AinhoaSalo@users.noreply.github.com>
2022-09-13 12:47:10 +0200

    Juego, no está terminado y me falta mucho. Hay errores

--- src/pages/Juego/Juego.jsx
+++ src/pages/Juego/Juego.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import cartaTrasera from './img/carta.png'
+import { useState, useEffect } from 'react'
+import { getPersonajes } from '../../services/commonService'
+import './Juego.css'
 
 export function Juego() {
+
+
+    useEffect(() => {
+        for (let i = 0; i < 16; i++) {
+            document.querySelector('.cartaTraseraJuego').innerHTML += `<img className='imgCartaTrasera' src=${cartaTrasera} alt="Carta Trasera" />`;
+        }
+
+    }, [])
+
+
+
     return (
         <>
-            <section>
+            <section className='delanteraTrasera'>
                 <div className='cartaTraseraJuego'></div>
-                <div className='cartaTraseradelantera'></div>
+                <div className='cartaDelanteraJuego'></div>
             </section>
         </>
     )
 }
+
+
