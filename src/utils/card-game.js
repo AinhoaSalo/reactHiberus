1fecbec3a393e2158e911ff604e4cee8eb09546c
Julen Mendioroz <jmendiorozredin@gmail.com>
2022-09-14 21:15:38 +0200

    ADD utils for card game

---
+++ src/utils/card-game.js
@@ -0,0 +1,15 @@
+import { getPersonajes } from "../services/commonService"
+
+const duplicate = (arr) => arr.concat(arr)
+const assignIds = (arr) => arr.map((obj, key) => ({ key, ...obj }))
+
+export const shuffle = (arr) => [...arr].sort(() => Math.random() - 0.5)
+export const getCards = async () => {
+  try {
+    const characters = await getPersonajes()
+    const duplicated = duplicate(characters.slice(0, 4))
+    return assignIds(duplicated)
+  } catch (error) {
+    return []
+  }
+}
