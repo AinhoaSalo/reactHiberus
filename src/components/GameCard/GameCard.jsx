b8dee31e3a1ddc4994bcfab205892b357ac0c53a
Julen Mendioroz <jmendiorozredin@gmail.com>
2022-09-14 21:16:52 +0200

    UPDATE game to first version

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,18 +1,11 @@
-import { useState } from "react"
 import "./GameCard.css"
 
-const frontImg =
-  "https://es.web.img3.acsta.net/pictures/18/10/31/17/34/2348073.jpg"
-
-export const GameCard = (props) => {
-  const [isFlipped, setIsFlipped] = useState(false)
-  const toggle = () => setIsFlipped(!isFlipped)
-
+export const GameCard = ({ image, isFlipped, flip }) => {
   return (
-    <div className={`game-card ${isFlipped ? "flipped" : ""}`} onClick={toggle}>
+    <div className={`game-card ${isFlipped ? "flipped" : ""}`} onClick={flip}>
       <div
         className="card-front"
-        style={{ backgroundImage: `url(${frontImg})` }}
+        style={{ backgroundImage: `url(${image})` }}
       ></div>
       <div className="card-back"></div>
     </div>
