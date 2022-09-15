bd969493283ca56e0b9a7479118349acc91d5fad
Luis <luisfaro013@gmail.com>
2022-09-15 14:13:01 +0200

    cambios luis

--- src/components/DetailViewEpisodioComponent/DetailViewEpisodioComponent.jsx
+++ src/components/DetailViewEpisodioComponent/DetailViewEpisodioComponent.jsx
@@ -13,6 +13,9 @@ export const DetailViewEpisodioComponent = ({ id, name, characters, created, air
           <li>Fecha de estreno: {air_date}</li>
           <li>Fecha de creación: {created}</li>
         </ul>
+        <br />
+        <h4>Resumen</h4>
+        <p className='texto'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, cumque explicabo expedita rem voluptatum esse inventore officia iure dolorum? Perferendis assumenda molestias dolorem commodi libero illum eum ad maiores dolor!</p>
       </div>
     </div>
 
--- src/pages/Personajes/Personajes.jsx
+++ src/pages/Personajes/Personajes.jsx
@@ -7,19 +7,30 @@ import { getPersonajes } from '../../services/commonService.js'
 
 export const Personajes = () => {
   const [personajes, setPersonajes] = useState([])
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     getPersonajes().then(results => {
+      console.log(results)
       setPersonajes(results)
-
     })
   }, [])
 
+  const searcher = (e) => {
+    setSearch(e.target.value)
+  }
+
   return (
     <div className='containerPersonajes'>
+      <div className='buscador'>
+        <label className='lab' htmlFor="search">Buscador:</label>
+        <input className='inp' value={search} onChange={searcher} type="text" placeholder='Search' id='search' />
+      </div>
       {
 
-        personajes.map(per => <Card key={per.id} name={per.name} image={per.image} id={per.id}/>)
+        personajes.filter(per =>
+          per.name.toLowerCase().includes(search.toLocaleLowerCase())
+        ).map(per => <Card key={per.id} name={per.name} image={per.image} id={per.id} />)
 
       }
     </div>
